818b7d02d8e1d10ad8305ed73206a91581b1a281
foysal41 <mdfoysal.mf33@gmail.com>
2024-04-02 01:49:44 +0600

    Retive the value from html input field

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $("#calculator-form").submit(function (event) {
     event.preventDefault();
+
+    let num1 = parseFloat($("#num1").val());
+    let num2 = parseFloat($("#num2").val());
+    let operation = $("#operation").val();
   });
 });
